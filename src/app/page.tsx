e32bfa393c6acfc49f535a901a632616dbd2e98e
jukangpark <skyxxx9339@naver.com>
2023-09-16 14:55:33 +0900

    홈페이지에, 예비 데이터 "/api/todos/" 경로로 POST 하는 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 import Navigation from "./ui/Navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    fetch("http://localhost:3000/api/hello")
+    fetch("/api/hello")
       .then((res) => res.json())
       .then((data) => console.log(data));
   });
+
+  const handlePost = () => {
+    fetch("/api/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: "test" }),
+    });
+  };
 
   return (
     <div>
@@ -18,6 +27,9 @@ export default function Home() {
       <Link href="/practice">practice</Link>
       <h4>네비게이션</h4>
       <Navigation />
+      <button onClick={handlePost}>
+        test 디비에 todos 콜렉션에 데이터 하나 넣겠습니다.
+      </button>
     </div>
   );
 }
